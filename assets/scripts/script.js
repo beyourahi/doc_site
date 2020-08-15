5cff501d6925e9d6e700ae4693cb62d103f1d73f
rahikhan <rahikhan360@gmail.com>
2020-08-16 04:57:04 +0600

    search bar updated

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,65 @@
-// !Variable Declarations
+//! Seach Bar Code
+timeout_var = null;
 
-// !Main Code
+function typeWriter(
+    selector_target,
+    text_list,
+    placeholder = false,
+    i = 0,
+    text_list_i = 0,
+    delay_ms = 200
+) {
+    if (!i) {
+        if (placeholder) {
+            document.querySelector(selector_target).placeholder = "";
+        } else {
+            document.querySelector(selector_target).innerHTML = "";
+        }
+    }
+    txt = text_list[text_list_i];
+    if (i < txt.length) {
+        if (placeholder) {
+            document.querySelector(selector_target).placeholder += txt.charAt(
+                i
+            );
+        } else {
+            document.querySelector(selector_target).innerHTML += txt.charAt(i);
+        }
+        i++;
+        setTimeout(
+            typeWriter,
+            delay_ms,
+            selector_target,
+            text_list,
+            placeholder,
+            i,
+            text_list_i
+        );
+    } else {
+        text_list_i++;
+        if (typeof text_list[text_list_i] === "undefined") {
+            setTimeout(
+                typeWriter,
+                delay_ms * 5,
+                selector_target,
+                text_list,
+                placeholder
+            );
+        } else {
+            i = 0;
+            setTimeout(
+                typeWriter,
+                delay_ms * 3,
+                selector_target,
+                text_list,
+                placeholder,
+                i,
+                text_list_i
+            );
+        }
+    }
+}
+
+text_list = ["Search..."];
+
+return_value = typeWriter("#searchbar", text_list, true);
